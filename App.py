719324a03b74576d59160cb309e5888a6c3fdfb7
Darren Turchiarelli <107463700+DarrenTurchiarelli@users.noreply.github.com>
2023-04-27 12:30:06 +1000

    Add files via upload

---
+++ App.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, render_template
+import praw
+import openai
+
+app = Flask(__name__)
+
+# Initialize the Reddit and OpenAI APIs
+reddit = praw.Reddit(client_id='your_client_id',
+                     client_secret='your_client_secret',
+                     user_agent='your_user_agent')
+
+openai.api_key = 'your_api_key'
+
+# Define a function to search Reddit and return the results
+def search_reddit(query, subreddit):
+    search_results = reddit.subreddit(subreddit).search(query)
+    results = []
+    for result in search_results:
+        results.append({
+            'title': result.title,
+            'text': result.selftext
+        })
+    return results
+
+# Define a function to generate a response using the OpenAI API
+def get_gpt_response(question):
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=question,
+        max_tokens=100
+    )
+    return response.choices[0].text.strip()
+
+# Define the homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Define a route to handle the form submission
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Get the user's question from the form
+    question = request.form['question']
+    
+    # Search Reddit for relevant results
+    reddit_results = search_reddit(question, 'learnpython')
+    
+    # Generate a response using the OpenAI API
+    gpt_response = get_gpt_response(question)
+    
+    # Render the results template with the Reddit and GPT results
+    return render_template('results.html', reddit_results=reddit_results, gpt_response=gpt_response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
