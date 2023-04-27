61ba96e191a36d2e85f0e9f808c4af36d4c9245a
Darren Turchiarelli <107463700+DarrenTurchiarelli@users.noreply.github.com>
2023-04-28 08:56:35 +1000

    Update App.py

--- App.py
+++ App.py
@@ -4,22 +4,32 @@ import openai
 
 app = Flask(__name__)
 
-# Initialize the Reddit and OpenAI APIs
-reddit = praw.Reddit(client_id='your_client_id',
-                     client_secret='your_client_secret',
-                     user_agent='Integrate Reddit API and ChatGPT API by u/dturchiarelli')
+# Define the API keys
+client_id      = 'your_client_id'
+client_secret  = 'your_client_secret'
+user_agent     = 'your_user_agent'
+openai_api_key = 'your_openai_api_key'
 
-openai.api_key = 'your_api_key'
+# Initialize the Reddit and OpenAI APIs
+reddit = praw.Reddit(client_id     = client_id,
+                     client_secret = client_secret,
+                     user_agent    = user_agent)
+
+openai.api_key = openai_api_key
 
 # Define a function to search Reddit and return the results
 def search_reddit(query, subreddit):
     search_results = reddit.subreddit(subreddit).search(query)
     results = []
+    count = 0
     for result in search_results:
+        if count == 5: # limit the results to 5
+            break
         results.append({
             'title': result.title,
             'text': result.selftext
         })
+        count += 1
     return results
 
 # Define a function to generate a response using the OpenAI API
